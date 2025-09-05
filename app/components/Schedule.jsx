79c577401df242bce9ef37d9c2ac67a7091446c1
Yassin Manita <yassinmanita12@gmail.com>
2025-09-05 22:15:58 +0100

    Update Address

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -16,8 +16,8 @@ export default function Schedule() {
             <div className="event-info-card">
               <div className="info-icon">📍</div>
               <h3>Venue</h3>
-              <p>Soviva Hotel & Resort</p>
-              <p>Port El Kantaoui, Hammam Sousse 4011 Tunisie</p>
+              <p>Amir Palace</p>
+              <p>Zone Touristique Bp 21 Aéroport, 5000 Monastir, Tunisia</p>
             </div>
             
             <div className="event-info-card">
@@ -33,11 +33,11 @@ export default function Schedule() {
             <div className="hotel-image">
               <img 
                 src="/hotel.jpg" 
-                alt="Soviva Hotel & Resort"
+                alt="Amir Palace"
                 className="hotel-img"
               />
               <a 
-                href="https://www.google.com/maps/place/Soviva+Hotel+%26+Resort/@35.829861,10.626313,17z/data=!3m1!4b1!4m9!3m8!1s0x1302756b4f8c7c0f:0x4e8c1b2f1e5e4b5a!5m2!4m1!1i2!8m2!3d35.8298567!4d10.6288933!16s%2Fg%2F11b8v5zv5w?entry=ttu" 
+                href="https://maps.app.goo.gl/V2FXrBPwZ4F1HN4n7" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="map-link"
@@ -46,14 +46,13 @@ export default function Schedule() {
               </a>
             </div>
             <div className="map-container">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.225306092092!2d10.626313!3d35.829861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1302756b4f8c7c0f%3A0x4e8c1b2f1e5e4b5a!2sSoviva%20Hotel%20%26%20Resort!5e0!3m2!1sen!2stn!4v1620000000000!5m2!1sen!2stn"
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.463367167191!2d10.734303999999998!3d35.7639925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13020cc0e66aa0ff%3A0xf1b4942694536653!2sAmir%20Palace!5e0!3m2!1sen!2stn!4v1757106799579!5m2!1sen!2stn"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
                 allowFullScreen=""
                 loading="lazy"
-                title="Soviva Hotel & Resort Location"
+                title=""Amir Palace Location"
               ></iframe>
             </div>
           </div>
